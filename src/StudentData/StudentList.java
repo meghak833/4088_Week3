544323e89d0c34b3109ed3837752d2ff5cb893e0
meghak833 <59581814+meghak833@users.noreply.github.com>
2020-01-23 14:57:43 -0500

    Update StudentList.java
    object of parttimestudent created

--- src/StudentData/StudentList.java
+++ src/StudentData/StudentList.java
@@ -12,6 +12,8 @@ public class StudentList {
      */
     public static void main(String[] args) {
         System.out.println("hello");
+        
+        ParttimeStudent ps=new ParttimeStudent();
     }
 
 }
