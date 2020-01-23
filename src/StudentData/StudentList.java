ec8cadb21ada8ea4d18480a5281ccc8a963d4da4
lewisc25 <59709761+lewisc25@users.noreply.github.com>
2020-01-23 15:02:11 -0500

    Update StudentList.java

--- src/StudentData/StudentList.java
+++ src/StudentData/StudentList.java
@@ -11,7 +11,7 @@ public class StudentList {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello there");
     }
 
 }
