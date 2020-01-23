8997808486883a424e83b026194933f6088acdd0
chris <lewis25@sheridancollege.ca>
2020-01-23 14:54:37 -0500

    PartTimeStudent class added

---
+++ src/StudentData/PartTimeStudent.java
@@ -0,0 +1,55 @@
+package StudentData;
+
+/**
+ * This class +++Insert Description Here+++
+ *
+ * @author Christopher Lewis
+ */
+public class PartTimeStudent 
+{
+    private int studId;
+    private String name;
+    private String programName;
+
+    /**
+     * @return the studId
+     */
+    public int getStudId() {
+        return studId;
+    }
+
+    /**
+     * @param studId the studId to set
+     */
+    public void setStudId(int studId) {
+        this.studId = studId;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the programName
+     */
+    public String getProgramName() {
+        return programName;
+    }
+
+    /**
+     * @param programName the programName to set
+     */
+    public void setProgramName(String programName) {
+        this.programName = programName;
+    }
+}
