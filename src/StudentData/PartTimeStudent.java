39046005d695134106c19a4f93fac21d6959906a
Ha Neul Moon <moonhan@sheridancollege.ca>
2020-01-23 14:56:00 -0500

    PartTimeStudent class getters and setters added

--- src/StudentData/PartTimeStudent.java
+++ src/StudentData/PartTimeStudent.java
@@ -6,10 +6,66 @@ package StudentData;
  * @author Ha Neul Moon
  */
 public class PartTimeStudent {
-    int studId;
-    String name;
-    String programName;
-    int programLength; // mth/yr[number]
+
+    /**
+     * @return the studId
+     */
+    public int getStudId() {
+        return studId;
+    }
+
+    /**
+     * @param studId the studId to set
+     */
+    public void setStudId(int studId) {
+        this.studId = studId;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the programName
+     */
+    public String getProgramName() {
+        return programName;
+    }
+
+    /**
+     * @param programName the programName to set
+     */
+    public void setProgramName(String programName) {
+        this.programName = programName;
+    }
+
+    /**
+     * @return the programLength
+     */
+    public int getProgramLength() {
+        return programLength;
+    }
+
+    /**
+     * @param programLength the programLength to set
+     */
+    public void setProgramLength(int programLength) {
+        this.programLength = programLength;
+    }
+    private int studId;
+    private String name;
+    private String programName;
+    private int programLength; // mth/yr[number]
 
     
 }
