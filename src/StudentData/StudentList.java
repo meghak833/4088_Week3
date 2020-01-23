417a82a92efc2e69a7a6c64bc14b2da7291ecd93
megha <megha.patel2@sheridancollege.ca>
2020-01-23 14:30:33 -0500

    initial code

---
+++ src/StudentData/StudentList.java
@@ -0,0 +1,17 @@
+
+package StudentData;
+
+/**
+ *
+ * @author Megha Patel
+ */
+public class StudentList {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+}
