b5cf7d1f2f8e5d7783b93b0dab34d56bdce753fd
Ha Neul Moon <moonhan@sheridancollege.ca>
2020-01-23 14:50:29 -0500

    PartTimeStudent class added

---
+++ src/StudentData/PartTimeStudent.java
@@ -0,0 +1,15 @@
+package StudentData;
+
+/**
+ * This class +++Insert Description Here+++
+ *
+ * @author Ha Neul Moon
+ */
+public class PartTimeStudent {
+    int studId;
+    String name;
+    String programName;
+    int programLength; // mth/yr[number]
+
+    
+}
